e207a48b6be269a71fb30264239e968f0deb5f5b
Renee Graham <renee.noluthando@gmail.com>
2021-04-02 15:02:36 +0100

    all tests apart from last

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -17,19 +17,54 @@ describe('calculator functionality', function() {
   })
 
 //   1. Do the number buttons update the display of the running total?
+it('should update display with running total of number number', function(){
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number1')).click();
+  element(by.css('#number2')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('12')
+})
 // 2. Do the arithmetical operations update the display with the result of the operation?
   it('should update display with operation result', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     element(by.css('#operator_add')).click();
-    element(by.css('#number_4')).click();
+    element(by.css('#number4')).click();
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('6')
   })
+  // 3. Can multiple operations be chained together?
+  it('should multiply operations when chained together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+    
   })
-// 3. Can multiple operations be chained together?
 // 4. Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
-// 5. What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
-
+it('should be the same output for  arange of numbers', function(){
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number2')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number8')).click();
+  element(by.css('#operator_subtract')).click();
+  element(by.css('#number1')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('-99.75')
+})
+// 5. What does the code do in exceptional circumstances? Specifically, 
+// if you divide by zero, what is the effect? Write a test to describe what you'd prefer 
+// to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
+it('should be the same output for  arange of numbers', function(){
+  running_total = element(by.css('#running_total'))
+  element(by.css('#number2')).click();
+  element(by.css('#operator_divide')).click();
+  element(by.css('#number0')).click();
+  element(by.css('#operator_equals')).click();
+  expect(running_total.getAttribute('value')).to.eventually.equal('Not a number')
+  })
 
 });
